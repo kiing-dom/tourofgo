310f9d6e2d315255039266c4c16b1dd5e2a182db
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-30 00:12:28 +0100

    named return values

--- basics/functions/main.go
+++ basics/functions/main.go
@@ -2,14 +2,30 @@ package main
 
 import "fmt"
 
+// integers
 func add(x int, y int) int {
-	return x + y;
+	return x + y
 }
 
 func add2(x, y int) int {
 	return x + y
 }
 
+// strings
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// named return values
+func split (sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
-	fmt.Println(add2(add(42, 13), 13)) // expected 68
+	fmt.Println(add2(add(42, 13), 13)) // expected: 68
+	fmt.Println(swap("hello", "world")) // expected: world hello
+
+	fmt.Println(split(20)) // expected: 8 12
 }
