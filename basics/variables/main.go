deb108f51e5574b965473591af3f5dd7ec6145b8
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-30 00:23:18 +0100

    variables with initialisers covered

--- basics/variables/main.go
+++ basics/variables/main.go
@@ -9,9 +9,16 @@ We see both in this example.
 */
 import "fmt"
 
+// var statement
 var c, python, java bool
+
+// variables with initializers
+var i, j int = 1, 2
 
 func main() {
 	var i int
 	fmt.Println(i, c, python, java)
+
+	var apple, banana, orange = true, false, "no!"
+	fmt.Println(i, j, apple, banana, orange)
 }
