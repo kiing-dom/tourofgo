d76f9ac398f4c498dcbfd694bc1fa47079886db5
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-30 00:03:49 +0100

    more functions

--- basics/functions/main.go
+++ basics/functions/main.go
@@ -6,6 +6,10 @@ func add(x int, y int) int {
 	return x + y;
 }
 
+func add2(x, y int) int {
+	return x + y
+}
+
 func main() {
-	fmt.Println(add(add(42, 13), 13)) // expected 68
+	fmt.Println(add2(add(42, 13), 13)) // expected 68
 }
