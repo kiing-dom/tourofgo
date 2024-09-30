b1f33e57c4abeee0665a8600d150d009f12a4f2d
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-30 18:31:01 +0100

    type conversions continued

--- basics/type-conversions/main.go
+++ basics/type-conversions/main.go
@@ -9,6 +9,7 @@ func main() {
 	var x, y int = 3, 4
 	var f float64 = math.Sqrt(float64(x * x + y * y))
 	var z uint = uint(f)
+	var f2 float64 = float64(z)
 
-	fmt.Printf("%v, %v, %v", x, y, z)
+	fmt.Printf("%v, %v, %v, %.1f", x, y, z, f2)
 }
