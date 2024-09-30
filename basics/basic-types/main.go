aef0d1fa69d73b5532e459aa26de7df461c72932
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-30 18:11:30 +0100

    basic types

--- basics/basic-types/main.go
+++ basics/basic-types/main.go
@@ -26,17 +26,19 @@ complex64 complex128
 The example shows variables of several types, and also that variable declarations may be "factored" into blocks
 ,as with import statements.
 
-The int, uint, and uintptr types are usually 32 bits wide on 32-bit systems and 64 bits wide on 64-bit systems. 
+The int, uint, and uintptr types are usually 32 bits wide on 32-bit systems and 64 bits wide on 64-bit systems.
 When you need an integer value you should use int unless you have a specific reason to use a sized or unsigned integer type.
 */
 
 var (
-	ToBe bool = false
-	MaxInt uint64 = 1<<64 - 1
-	z complex128 = cmplx.Sqrt(-5 + 12i)
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
-	func main() {
-		fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
-	}
-
+func main() {
+	TypeAndValue := "Type: %T Value: %v\n"
+	fmt.Printf(TypeAndValue, ToBe, ToBe)
+	fmt.Printf(TypeAndValue, MaxInt, MaxInt)
+	fmt.Printf(TypeAndValue, z, z)
+}
