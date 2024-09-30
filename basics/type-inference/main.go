b73f6bae38d629eb8ee52451efa7b0cb003b7f40
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-30 18:40:11 +0100

    more type inference examples

--- basics/type-inference/main.go
+++ basics/type-inference/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	v := 42
-	
+	v2 := math.Pi
+	v3 := 0.867 + 0.5i
+	v4 := "https://www.youtube.com/@1KIINGDOM"
 
 	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("v2 is of type %T\n", v2)
+	fmt.Printf("v3 is of type %T\n", v3)
+	fmt.Printf("v4 is of type %T\n", v4)
 }
