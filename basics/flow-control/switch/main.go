cf8e67a10026ffd914dac4c248c6e6e995489083
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-03 14:05:09 +0100

    switch statement with no condition

--- basics/flow-control/switch/main.go
+++ basics/flow-control/switch/main.go
@@ -3,7 +3,21 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
+
+// switch with no condition (same as saying switch true)
+func greeting() {
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good Morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good Afternoon!")
+	default:
+		fmt.Println("Good Evening!")
+	}
+}
 
 func main() {
 	fmt.Print("Go runs on ")
@@ -16,4 +30,5 @@ func main() {
 		//return the os
 		fmt.Printf("%s.\n", os)
 	}
+	greeting()
 }
