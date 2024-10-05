3a02350c47100cd04e341d71d436d5180111d90a
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-05 13:13:34 +0100

    covered pointers to structs

--- basics/more-types/structs/main.go
+++ basics/more-types/structs/main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 // A struct is a collection of fields
 // struct fields are accessed using a dot
+// struct fields can be accessed through struct pointers
+
 type Vertex struct {
 	X int
 	Y int
@@ -14,5 +16,8 @@ func main() {
 	fmt.Println(v)
 	// accessing struct fields
 	v.X = 4
+	//pointers to structs
+	p := &v
+	p.X = 1e9
 	fmt.Println(v)
 }
