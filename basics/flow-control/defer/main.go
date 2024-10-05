248fbc24182012a0fd76d5d956a13411d56ebe2c
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-05 12:52:01 +0100

    defer explanation

--- basics/flow-control/defer/main.go
+++ basics/flow-control/defer/main.go
@@ -9,6 +9,11 @@ The deferred call's arguments are evaluated immediately,
 but the function call is not executed until the surrounding function returns.
 */
 
+
+/*
+Deferred function calls are pushed onto a stack. When a function returns, 
+its deferred calls are executed in last-in-first-out order.
+*/
 func stackDefer() {
 	fmt.Println("Counting...")
 
