583e8daeb914e15a691b8f295b6e008d73e8ca15
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-19 08:27:51 +0100

    skipping index or value using range in go

--- basics/more-types/range/main.go
+++ basics/more-types/range/main.go
@@ -8,4 +8,13 @@ func main() {
 	for i, v := range pow {
 		fmt.Printf("2**%d = %d\n", i, v)
 	}
+
+	innerPow := make([]int, 10)
+	for i := range innerPow {
+		innerPow[i] =  1 << uint(i) // == 2** i
+	}
+
+	for _, value := range innerPow {
+		fmt.Printf("%d\n", value)
+	}
 }
